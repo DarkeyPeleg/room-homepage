229fb1e27de5b63eb28aa9ec9ae34af357f41aa2
DarkeyPeleg <peldarkey@gmail.com>
2024-05-19 17:50:24 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./componets/Header";
+import Showcase from "./componets/Showcase";
+import SectionTwo from "./componets/SectionTwo";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Showcase />
+      <SectionTwo />
+    </>
+  );
+}
+
+export default App;
